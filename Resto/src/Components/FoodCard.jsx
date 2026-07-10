b1a84f94e8769a8a274ad8ea99c3abc78d34891f
moetaz <rassaamoetaz@gmail.com>
2026-07-10 21:56:43 +0200

    as

--- Resto/src/Components/FoodCard.jsx
+++ Resto/src/Components/FoodCard.jsx
@@ -4,7 +4,7 @@ function FoodCard(props) {
   return (
     <div className='foodcard'>
       <h3>{props.food.name}</h3>
-    <img scr={props.food.images[0]}/>
+    <img src={props.food.images[0]}/>
     <h4>{props.food.price}DT</h4>
     </div>
   )
--- Resto/src/Components/Menu.jsx
+++ Resto/src/Components/Menu.jsx
@@ -9,24 +9,24 @@ function Menu(props) {
        <nav className='sidebar'>
          <NavLink className="navlink"   to="/menu/All">All</NavLink> 
           <NavLink className="navlink" to="/menu/Pizza">Pizza</NavLink>  
-          <NavLink className="navlink" to="/menu/sandwitch">Sandwitch</NavLink>  
-          <NavLink className="navlink" to="/menu/drinks">Drinks</NavLink> 
+          <NavLink className="navlink" to="/menu/sandwich">Sandwitch</NavLink>  
+          <NavLink className="navlink" to="/menu/drink">Drinks</NavLink> 
        </nav>
         <div className='productsmenu'>
            <Routes>
                 <Route path="/:categoryName" element={<FilteredProductsList list={list} />} />
-                <Route path="/" element={<FilteredProductsList list={list} categoryDefault="all" />} />
+                <Route path="/" element={<FilteredProductsList list={list}  />} />
             
         </Routes>  
         </div>  
 
     </div>
   )}
-  function FilteredProductsList({ list, categoryDefault }) {
+  function FilteredProductsList({ list}) {
     
     let { categoryName } = useParams(); 
     
-    const activeCat = categoryName || categoryDefault || "all";
+    const activeCat = categoryName || "all";
 
     
     const displayedItems = activeCat.toLowerCase() === "all"? list: list.filter(item => item.category.toLowerCase() === activeCat.toLowerCase());
